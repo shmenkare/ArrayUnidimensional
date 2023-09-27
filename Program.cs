48853bc44e712e4703b94b864a3ac87dcd1cd27b
“shmenkare” <shmenkare@gmail.com>
2023-09-27 14:35:05 -0400

    errores corregidos

--- Program.cs
+++ Program.cs
@@ -10,8 +10,8 @@ namespace ArrayUnidimensional
         static void Main(string[] args)
         {
             int i = 0;
-            int j = 1;
-            char l = ' ';
+            int j = 0;
+            char l = 'a';
             int m = 0;
             int x = 0;
             Console.WriteLine("Dame un numero para el array");
@@ -25,32 +25,37 @@ namespace ArrayUnidimensional
             }
             foreach (int elemento in numeros)
             {
-                
+                Thread.Sleep(150);
                 Console.Write(elemento+" ");
             }
             Console.WriteLine();
 
-            Console.WriteLine("Dame una letra para el array");
-            char c = Convert.ToChar(Console.ReadLine());
-
-            char[] alfabeto = new char[c];
-
-            for (l = 'a'; l <= c; l++)
+            Console.WriteLine("Ahora el alfabeto");
+            char[] alfabeto = new char[26];
+            //char[] alfabeto = {'a','e','i','o','u'};
+            //alfabeto[0] = 'a';
+            Console.WriteLine();
+            for (m =0; m < 26; m++)
             {
-                alfabeto[m++] = l;
+                Thread.Sleep(150);
+                Console.Write(m);
+                Console.Write(l + " ");
+                alfabeto[m] = l++;
             }
+            Console.WriteLine();
             foreach (char letras in alfabeto)
             {
-
+                Thread.Sleep(150);
                 Console.Write(letras + " ");
             }
+            Console.WriteLine();
+
             for (x = 0; x < alfabeto.Length; x++)
             {
-
+                Thread.Sleep(150);
+                Console.Write(x);
                 Console.Write(alfabeto[x] + " ");
             }
-
-
             Console.ReadKey();
             
 
